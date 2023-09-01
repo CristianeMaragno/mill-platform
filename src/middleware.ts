e48768dba59cc28769a2e3abc2d7c27a263fbc8d
Cristiane R. Maragno <cristianerodriguesmaragno@gmail.com>
2023-09-01 16:26:13 -0300

    Add fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,19 +5,20 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-    afterAuth(auth, req, evt) {
-        // handle users who aren't authenticated
-        if (!auth.userId && !auth.isPublicRoute) {
-          return redirectToSignIn({ returnBackUrl: req.url });
-        }
-        // redirect them to organization selection page
-        if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/dashboard"){
-          const orgSelection = new URL('/dashboard', req.url)
-          return NextResponse.redirect(orgSelection)
-        }
-    },
+	afterAuth(auth, req, evt) {
+		// handle users who aren't authenticated
+		if (!auth.userId && !auth.isPublicRoute) {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+			return redirectToSignIn({ returnBackUrl: req.url });
+		}
+		// redirect them to organization selection page
+		if(auth.userId && !auth.orgId && req.nextUrl.pathname !== "/dashboard"){
+			const orgSelection = new URL('/dashboard', req.url)
+			return NextResponse.redirect(orgSelection)
+		}
+	},
 
-    publicRoutes: ["/"]
+	publicRoutes: ["/"]
 });
 
 export const config = {
