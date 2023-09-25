cd29e27fd3bd6aeafee5c60670883bcec763eea6
Cristiane R. Maragno <cristianerodriguesmaragno@gmail.com>
2023-09-25 09:20:56 -0300

    Add google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import { type AppType } from "next/app";
 import { ClerkProvider } from '@clerk/nextjs'
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import GoogleAnalytics from '~/components/googleAnalytics';
+import CookieBanner from '~/components/cookieBanner';
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return  <ClerkProvider {...pageProps}>
-    <Component {...pageProps} />
-  </ClerkProvider>
+  return (
+    <ClerkProvider {...pageProps}>
+      <GoogleAnalytics GA_MEASUREMENT_ID='G-8BXTSKELP2'/>
+      <CookieBanner/>
+      <Component {...pageProps} />
+    </ClerkProvider>
+  );
 
 };
 
